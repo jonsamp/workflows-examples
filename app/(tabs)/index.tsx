888a755b9b3c029f72dcf710993eae0e5fc92986
Jon Samp <sampjon@gmail.com>
2024-12-20 16:32:31 -0600

    update 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! Update 1</ThemedText>
+        <ThemedText type="title">Welcome! Update 2</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
